66232913b82f14fd4866f070384f5f6515f18d22
Garrett <Garrett@YUP>
2024-05-20 14:46:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Assignment3._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetEvenNumbers());
+        }
+
+
+
+
+
+          public class EvenNumbers;
+        
+
+        
+        
+
+            public static string GetEvenNumbers()
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sb.Append(i).Append(" ");
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+
+    }
+
+
+        
+    
+
